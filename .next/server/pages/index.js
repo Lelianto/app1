f60102686b89e7c7c412b3523d3443da5470c69f
Lelianto Eko Pradana <lelianto.eko@gmail.com>
2023-02-24 20:05:00 +0700

    first commit

---
+++ .next/server/pages/index.js
@@ -0,0 +1,36 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(function() {
+var exports = {};
+exports.id = "pages/index";
+exports.ids = ["pages/index"];
+exports.modules = {
+
+/***/ "react":
+/*!*******************************************************************************************************!*\
+  !*** external "/Users/lelianto/Documents/mf/app1/node_modules/@module-federation/nextjs-mf/react.js" ***!
+  \*******************************************************************************************************/
+/***/ (function(module) {
+
+"use strict";
+module.exports = require("/Users/lelianto/Documents/mf/app1/node_modules/@module-federation/nextjs-mf/react.js");;
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+var __webpack_exports__ = __webpack_require__.X(0, ["vendors-node_modules_object-assign_index_js","vendors-node_modules_react_jsx-dev-runtime_js","pages_index_tsx"], function() { return __webpack_exec__("./pages/index.tsx"); });
+module.exports = __webpack_exports__;
+
+})();
